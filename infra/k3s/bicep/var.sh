a4daef69a89a8a232125fa433a19ff70e932cdfa
Brian Redmond <brianisrunning@gmail.com>
2021-06-30 12:27:22 -0600

    how now brown cow

--- infra/k3s/bicep/var.sh
+++ infra/k3s/bicep/var.sh
@@ -3,7 +3,7 @@
 # Set Variables
 export ARM_DEPLOYMENT_NAME="reddogbicep"
 #export BICEP_FILE="deploy.bicep"
-export SUBSCRIPTION="471d33fd-a776-405b-947c-467c291dc741"
+export SUBSCRIPTION=""
 export TENANT_ID="72f988bf-86f1-41af-91ab-2d7cd011db47"
 export K3S_TOKEN='CAa6BYPyp+6NwLY5f3or'
 export ADMIN_USER_NAME='reddogadmin'
