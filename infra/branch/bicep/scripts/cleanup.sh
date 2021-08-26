924f16ca8c18b333ec6a05a7d72cfe6fef280b4b
Ray Kao <ray@peopleandcode.com>
2021-08-26 11:07:06 -0400

    fixing cleanup scripts for each unit

--- infra/branch/bicep/scripts/cleanup.sh
+++ infra/branch/bicep/scripts/cleanup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+source ./vars.sh
+
 # Delete SSH Keys
 rm -rf ssh_keys
 
 # Delete logs
 rm -rf logs
 
-export RG_PREFIX="$(cat infra.json|jq -r '.rgPrefix')"
-
 # Delete Resource Groups
-for branch in $(cat infra.json|jq -c '.branches[]')
+for branch in $BRANCHES
 do
 export BRANCH_NAME=$(echo $branch|jq -r '.branchName')
 export RG_LOCATION=$(echo $branch|jq -r '.location')
-export RG_NAME=$RG_PREFIX-$BRANCH_NAME-$RG_LOCATION
+export RG_NAME=$PREFIX-$BRANCH_NAME-$RG_LOCATION
 
 # Create Branch
 echo "Deleting Resource Group: $RG_NAME"
--- infra/hub/bicep/scripts/cleanup.sh
+++ infra/hub/bicep/scripts/cleanup.sh
@@ -1,7 +1,5 @@
-export PREFIX=$(cat infra.json|jq -r '.hub.rgNamePrefix')
-export RG_LOCATION=$(cat infra.json|jq -r '.hub.location')
-export RG_NAME=$PREFIX-hub-$RG_LOCATION
-
-az group delete -n $RG_NAME -y --no-wait
+source ./var.sh
 
 rm -rf ssh_keys
+
+az group delete -n $RG_NAME -y --no-wait
