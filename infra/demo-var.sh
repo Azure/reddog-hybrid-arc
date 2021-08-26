02754af3d358bc06981c3a245c312211f9535474
Ray Kao <ray@peopleandcode.com>
2021-08-25 23:39:34 -0400

    preparing for 'super script' run/deploy

---
+++ infra/demo-var.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Set Variables
+export ARM_DEPLOYMENT_NAME="reddogbicep"
+export SUBSCRIPTION_ID="62afe9fc-190b-4f18-95ac-e5426017d4c8"
+export TENANT_ID="72f988bf-86f1-41af-91ab-2d7cd011db47"
+export K3S_TOKEN='CAa6BYPyp+6NwLY5f3or'
+export ADMIN_USER_NAME='reddogadmin'
+export SSH_KEY_PATH="./ssh_keys"
+export RABBIT_MQ_PASSWD='MyPassword123'
+export REDIS_PASSWD='MyPassword123'
+
+export PREFIX=$(cat infra.json|jq -r '.rgNamePrefix')
+export HUBNAME=$(cat infra.json|jq -r '.hub.hubName')
+
+export RG_LOCATION=$(cat infra.json|jq -r '.hub.location')
+export RG_NAME=reddog-$PREFIX-$HUBNAME-$RG_LOCATION
+
+export BRANCHES=$(cat demo-infra.json|jq -c '.branches[]')
