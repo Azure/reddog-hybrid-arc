420d8b28b3a50f7528d8a3226079f95b0197db54
Ray Kao <ray@peopleandcode.com>
2021-09-07 00:58:26 -0400

    ensure outputs get deleted for super deploy

--- infra/cleanup.sh
+++ infra/cleanup.sh
@@ -8,6 +8,9 @@ rm -rf $SSH_KEY_PATH
 # Delete logs
 rm -rf logs
 
+# Delete outputs
+rm -rf outputs
+
 # Delete Hub Resource Group
 echo "Deleting Resource Group: $RG_NAME"
 az group delete -n $RG_NAME -y --no-wait
